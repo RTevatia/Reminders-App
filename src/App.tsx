4cc269886379ac70c886f059fdf3c0bbd33794e1
Rahul Tevatia <rtevatia2@gmail.com>
2026-02-11 21:41:15 -0500

    Implement state management using useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import ReminderList from "./Components/ReminderList";
 import Reminder from "./Models/reminder";
 
+
 function App() {
-  const reminders: Reminder[] = [{ id: 1, title: "Reminder 1" }];
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: "Reminder 1" },
+  ]);
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
